87e326e58759c684d0f60af5c4ae7bbb95ec9dfb
szpaladawid-ship-it <szpaladawid@gmail.com>
2026-07-31 21:46:56 +0200

    test(foundation): include architecture contracts

--- tests/foundation/index.ts
+++ tests/foundation/index.ts
@@ -4,5 +4,6 @@ import "./governance.test";
 import "./observability.test";
 import "./sync.test";
 import "./accessibility.test";
+import "./architecture.test";
 
 console.log("All foundation contract tests passed.");
