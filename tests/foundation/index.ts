7cf8885dddeab6a29bc189e899e89bc38e673171
szpaladawid-ship-it <szpaladawid@gmail.com>
2026-07-31 21:18:48 +0200

    test(accessibility): include atom 21 foundation suite

--- tests/foundation/index.ts
+++ tests/foundation/index.ts
@@ -3,5 +3,6 @@ import "./entitlements.test";
 import "./governance.test";
 import "./observability.test";
 import "./sync.test";
+import "./accessibility.test";
 
 console.log("All foundation contract tests passed.");
